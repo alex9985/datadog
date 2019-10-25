180382469bcffa1c311bc92bcee57bc7580c2931
alex <alex@alexs-Mac-mini.joker>
2019-10-25 15:53:10 -0700

    added ram metrics

--- update.sh
+++ update.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 $path=/etc/datadog-agent/conf.d/snmp.d
 echo 'cleaning up work directory /etc/datadog-agent/conf.d/snmp.d'
-cd /etc/datadog-agent/conf.d/snmp.d
+cd $path
     for i in $(find -name "*.yaml"); 
         do sudo rm $i; 
         echo 'deleted ' + $i
     done
 sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/freenas.yaml -o pfsense.yaml
 sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/freenas.yaml -o freenas.yaml
+sudo service datadog-agent restart
