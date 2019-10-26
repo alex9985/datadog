38718092aae80592ef7e5b380bf94d43014b522d
alex <alex@alexs-Mac-mini.joker>
2019-10-25 17:10:34 -0700

    storagegroup

--- update.sh
+++ update.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-echo 'cleaning up work directory /etc/datadog-agent/conf.d/snmp.d'
 cd /etc/datadog-agent/conf.d/snmp.d
     for i in $(find -name "*.yaml"); 
         do sudo rm $i; 
         echo 'deleted ' + $i
     done
-sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/freenas.yaml -o pfsense.yaml
-sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/freenas.yaml -o reenas.yaml
+sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/pfsense.yaml -o pfsense.yaml
+sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/freenas.yaml -o freenas.yaml
 sudo service datadog-agent restart
