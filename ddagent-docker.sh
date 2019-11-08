bde9135db68ac6d600dd42b229d4d205a26a8ded
alex9985 <49490201+alex9985@users.noreply.github.com>
2019-11-07 20:16:06 -0800

    Create ddagent-docker.sh

---
+++ ddagent-docker.sh
@@ -0,0 +1,1 @@
+DOCKER_CONTENT_TRUST=1 docker run -d --name dd-agent -v /var/run/docker.sock:/var/run/docker.sock:ro -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e DD_API_KEY=cfbff7ee70f2ee29827776ff538ab461 datadog/agent:6.12.1
