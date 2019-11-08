3d6185438298363b6dc2fb12cd859e95fde43458
alex9985 <49490201+alex9985@users.noreply.github.com>
2019-11-08 08:13:29 -0800

    Delete create-container.sh

--- create-container.sh
+++
@@ -1,7 +0,0 @@
-function create_container {
-    image=$(docker run -d --name dd-agent -v /var/run/docker.sock:/var/run/docker.sock:ro -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e DD_API_KEY= datadog/agent:6.12.1)
-    sudo  docker cp /tmp/pfsense.yaml $image:/etc/datadog-agent/conf.d/snmp.d
-    sudo  docker cp /tmp/freenas.yaml $image:/etc/datadog-agent/conf.d/snmp.d
-    sudo docker restart $image
-    }
-create_container
