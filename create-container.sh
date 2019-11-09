944176be93967f33c6cb38e2d451bd9389b06146
alex <alex@alexs-mac-mini.joker>
2019-11-09 14:50:45 -0800

    updated config file

--- create-container.sh
+++ create-container.sh
@@ -15,10 +15,17 @@ echo "finished cleaning docker"
 cd /tmp/datadog
 sudo chmod +x create-container.sh
 #
+echo "enter DD API key"
+read apikey
+#
 function create_container {
         for file in /tmp/templates/*; do
                 echo "starting new container "
-                image=$(sudo docker run -d  -v /var/run/docker.sock:/var/run/docker.sock:ro -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e DD_API_KEY= datadog/agent:6.12.1)
+                image=$(sudo docker run -d  -v /var/run/docker.sock:/var/run/docker.sock:ro 
+                -v /proc/:/host/proc/:ro 
+                -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro 
+                -e DD_API_KEY=$apikey
+                datadog/agent:6.12.1)
                 echo "copying yaml templates"
                 sudo  docker cp /tmp/templates/pfsense.yaml $image:/etc/datadog-agent/conf.d/snmp.d
                 sudo  docker cp /tmp/templates/freenas.yaml $image:/etc/datadog-agent/conf.d/snmp.d
