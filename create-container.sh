b4c268c064bef39ac295115de4b750e1c7d27e22
alex <alex@alexs-mac-mini.joker>
2019-11-09 12:03:08 -0800

    updated container config

--- create-container.sh
+++ create-container.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 function create_container {
     image=$(docker run -d --name dd-agent -v /var/run/docker.sock:/var/run/docker.sock:ro -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e DD_API_KEY=81dd83da39b9c16e3931c48091612016 datadog/agent:6.12.1)
-    sudo  docker cp /tmp/pfsense.yaml 29a09dba0e86:/etc/datadog-agent/conf.d/snmp.d
-    sudo  docker cp /tmp/freenas.yaml 29a09dba0e86:/etc/datadog-agent/conf.d/snmp.d
+    sudo  docker cp /tmp/pfsense.yaml $image:/etc/datadog-agent/conf.d/snmp.d
+    sudo  docker cp /tmp/freenas.yaml $image:/etc/datadog-agent/conf.d/snmp.d
     sudo docker restart $image
     }
 create_container
--- update_config.sh
+++ update_config.sh
@@ -5,9 +5,9 @@ cd /tmp
         do sudo rm $i; 
         echo 'deleted ' + $i
     done;
-sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/pfsense.yaml -o /tmp/pfsense.yaml;
-sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/freenas.yaml -o /tmp/freenas.yaml;
-sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/create-container.sh -o /tmp/create-container.sh;
+sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/pfsense.yaml?token=ALZSSGLH2PDP3A5YHHSHSAC5Y4NS6 -o /tmp/pfsense.yaml;
+sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/freenas.yaml?token=ALZSSGNJJQXGTH7EPHRLJL25Y4NUG -o /tmp/freenas.yaml;
+sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/create-container.sh?token=ALZSSGNBC6DGI3TXFQWWQEC5Y4NQG -o /tmp/create-container.sh;
 
 while :
 do
