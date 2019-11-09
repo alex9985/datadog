1e81d57670846044797a5d1a34d6bae6061aff1a
alex <alex@alexs-mac-mini.joker>
2019-11-09 13:37:46 -0800

    updated container file

--- create-container.sh
+++ create-container.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+cd /tmp/templates
+    for i in $(find -name "*.yaml"); 
+        do sudo rm $i; 
+        echo 'deleted ' + $i
+    done;
+sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/pfsense.yaml -o /tmp/templates/pfsense.yaml;
+sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/freenas.yaml -o /tmp/templates/freenas.yaml;
+#
+echo "removing old conteiners"
+sudo docker rm -f $(sudo docker ps -qa)
+echo "finished cleaning docker"
+cd /tmp/datadog
+sudo chmod +x create-container.sh
+#
 function create_container {
-    echo "enter DataDog API key "
-    read apikey
         for file in /tmp/templates/*; do
-                echo "starting new container with $apikey"
-                image=$(sudo docker run -d  -v /var/run/docker.sock:/var/run/docker.sock:ro -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e DD_API_KEY=$apikey datadog/agent:6.12.1)
+                echo "starting new container "
+                image=$(sudo docker run -d  -v /var/run/docker.sock:/var/run/docker.sock:ro -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e DD_API_KEY=81dd83da39b9c16e3931c48091612016 datadog/agent:6.12.1)
                 echo "copying yaml templates"
                 sudo  docker cp /tmp/templates/pfsense.yaml $image:/etc/datadog-agent/conf.d/snmp.d
                 sudo  docker cp /tmp/templates/freenas.yaml $image:/etc/datadog-agent/conf.d/snmp.d
--- update_config.sh
+++ update_config.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-cd /tmp/templates
-    for i in $(find -name "*.yaml"); 
-        do sudo rm $i; 
-        echo 'deleted ' + $i
-    done;
-sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/pfsense.yaml -o /tmp/templates/pfsense.yaml;
-sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/freenas.yaml -o /tmp/templates/freenas.yaml;
 #sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/create-container.sh -o /tmp/datadog/create-container.sh;
 echo "removing old conteiners"
 sudo docker rm -f $(sudo docker ps -qa)
