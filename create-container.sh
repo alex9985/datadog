1eaddaf7ca225e6c6679f1327058e793416bd828
alex <alex@alexs-mac-mini.joker>
2019-11-09 13:17:17 -0800

    updated containter file

--- create-container.sh
+++ create-container.sh
@@ -4,7 +4,7 @@ function create_container {
     read apikey
         for file in /tmp/templates/*; do
                 echo "starting new container with $apikey"
-                image=$(sudo docker run -d  -v /var/run/docker.sock:/var/run/docker.sock:ro -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e DD_API_KEY=$apikey  datadog/agent:6.12.1)
+                image=$(sudo docker run -d  -v /var/run/docker.sock:/var/run/docker.sock:ro -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e DD_API_KEY=$apikey datadog/agent:6.12.1)
                 echo "copying yaml templates"
                 sudo  docker cp /tmp/templates/pfsense.yaml $image:/etc/datadog-agent/conf.d/snmp.d
                 sudo  docker cp /tmp/templates/freenas.yaml $image:/etc/datadog-agent/conf.d/snmp.d
