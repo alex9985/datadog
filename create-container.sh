86fac03b28e57c024b3c9cce7503fc5f4018a5bb
alex <alex@alexs-mac-mini.joker>
2019-11-15 17:57:53 -0800

    updated cisco yaml

--- create-container.sh
+++ create-container.sh
@@ -6,14 +6,13 @@ cd /tmp/templates
         do sudo rm $i; 
         echo 'deleted ' + $i
     done;
-sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/pfsense.yaml -o /tmp/templates/pfsense.yaml;
-sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/freenas.yaml -o /tmp/templates/freenas.yaml;
-sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/cisco-sw.yaml -o /tmp/templates/cisco-sw.yaml;
+sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/pfsense.yaml -o pfsense.yaml;
+sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/freenas.yaml -o freenas.yaml;
+sudo curl https://raw.githubusercontent.com/alex9985/datadog/master/cisco-sw.yaml -o cisco-sw.yaml;
 #
 echo "removing old conteiners"
 sudo docker rm -f $(sudo docker ps -qa)
 echo "finished cleaning docker"
-cd /tmp/datadog
 #sudo chmod +x create-container.sh
 #
 #echo "enter DD API key"
@@ -23,9 +22,8 @@ function create_container {
         for file in /tmp/templates/*; do
                 echo "starting new container "
                 image=$(sudo docker run -d  -v /var/run/docker.sock:/var/run/docker.sock:ro -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e DD_API_KEY= datadog/agent:6.12.1)
-                echo "copying yaml templates"
-                sudo  docker cp /tmp/templates/pfsense.yaml $image:/etc/datadog-agent/conf.d/snmp.d
-                sudo  docker cp /tmp/templates/freenas.yaml $image:/etc/datadog-agent/conf.d/snmp.d
+                echo "copying yaml templates to $file"
+                sudo  docker cp $file $image:/etc/datadog-agent/conf.d/snmp.d
                 sudo docker restart $image
         done
                           }
